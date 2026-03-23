cf93150ea2ff3c211fcb871260e4e47d8d8c6fe9
lulla029 <wu1154@mcmaster.ca>
2026-03-23 13:41:55 -0400

    Update bridge.py
    this python bridge includes request module to connect to the wifi

--- bridge.py
+++ bridge.py
@@ -1,36 +1,26 @@
-import serial # pyserial: allows Python talk to COM ports (Arduino)
-import asyncio # built-in: event loops, sleeps
-import websockets # interactive applications. Python can send commands and interact with the arduino
-
-# Added error handling because the script crashed when COM6 was unplugged
-try:
-    ser = serial.Serial('COM6', 115200, timeout=1)
-    print("Connected to Arduino on COM6")
-except Exception as e:
-    # if opening port fails (wrong port, permission issue).
-    print("ERROR: Arduino not found. Check your cable!")
-    print(e)
+import requests
+import asyncio
+import websockets
 
 async def handler(websocket):
     while True:
         try:
-            # Added check to see if serial exists before reading
+            RES = requests.get("http://192.168.4.1/sensor")
 
-            if 'ser' in locals() and ser.in_waiting > 0:
-                # Read a line from serial, decode bytes to text, and strip newline
-                data = ser.readline().decode('utf-8').strip()
-                # send line to connected websocket client
+            if RES.status_code == 200:
+                data = RES.text
+                print(f"Sensor: {data}")
                 await websocket.send(data)
-        except:
-            pass
-        await asyncio.sleep(0.05) 
-
-# start a websocket server at localhost:8031
-# run forvever
+            else:
+                print(f"Server responds with status code: {RES.status_code}")
+        except requests.ConnectionError:
+            print("Connection time out")
+        await asyncio.sleep(0.01)
+ 
 async def main():
+    print("Bridge running on wifi... Refresh your website!")
     async with websockets.serve(handler, "localhost", 8081):
         await asyncio.Future()
-
-
+ 
 if __name__ == "__main__":
     asyncio.run(main())
